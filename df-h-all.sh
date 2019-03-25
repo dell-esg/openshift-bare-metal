e047c4fab04d2374268193e23ec5a493093206ce
root <root@bastion.r5a.local>
2019-03-25 13:25:25 -0500

    add new files

---
+++ df-h-all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+HOSTS=(master1 master2 master3 infra1 infra2 infra3 app1 app2 app3 app4 stor1 stor2 stor3 stor4)
+for host in ${HOSTS[*]}
+do
+    echo $host
+    ssh $host "df -h"
+done
