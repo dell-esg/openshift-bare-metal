5f79aa6d6acaef9830f89c3edf239a9d8feae672
usunnapu <usunnapu@gmail.com>
2020-08-11 19:41:55 -0400

    modularized nodes info

--- python/generate_inventory_file.py
+++ python/generate_inventory_file.py
@@ -16,6 +16,8 @@ from helper import create_dir, check_path, get_ip, get_network_device_mac, \
                    map_interfaces_network, get_idrac_creds, generate_network_devices_menu, \
                    get_device_enumeration, get_user_response, get_mac_address
 
+from nodes import get_nodes_info
+
 class InventoryFile:
     def __init__(self, inventory_dict = {}):
         self.inventory_dict = inventory_dict
@@ -178,75 +180,7 @@ class InventoryFile:
 
         """
         self.clear_screen()
-        default = 3
-        master_nodes_count = input('enter number of master nodes\n'
-                                   'default [3]: ')
-        master_nodes_count = set_values(master_nodes_count, default, check='integer')
-        self.inventory_dict['csah']['vars']['master_nodes'] = []
-        bonding = input('Do you want to perform bonding (y/NO): ')
-        valid_responses = ['y', 'NO']
-
-        while bonding not in valid_responses:
-            logging.info('Invalid option provided. Enter \'y\' or \'NO\'')
-            bonding = input('Do you want to perform bonding (y/NO): ')
-
-        for num in range(master_nodes_count):
-            master_values = []
-            master_mac = ''
-            master_devices = None
-            default = 'etcd-{}'.format(num)
-            master_name = input('enter the master {} node name \n'
-                                'default [{}]: '.format(num, default))
-            master_name = set_values(master_name, default)
-
-
-            master_os_ip = get_ip(node_name=master_name, ip_type='os')
-            master_os_ip = validate_ip(master_os_ip)
-            master_idrac_ip = get_ip(node_name=master_name, ip_type='idrac')
-            response = check_ip_ping(master_idrac_ip)
-            
-            if response != 0:
-                get_user_response(message='idrac ip {} not pingeable'.format(master_idrac_ip))
-            else:
-                user, passwd = get_idrac_creds(master_idrac_ip)
-                base_api_url = 'https://{}/redfish/v1/Systems/System.Embedded.1/EthernetInterfaces'.format(master_idrac_ip)
-                master_devices = get_network_devices(user, passwd, base_api_url)
-
-            if master_devices:
-                map_devices = map_interfaces_network(master_devices)
-
-            if map_devices:
-                if bonding == 'y':
-                    master_mac = get_network_device_mac(map_devices, user, passwd, base_api_url)
-                    master_bond_name = 'bond0'
-                    master_active_bond_device = generate_network_devices_menu(map_devices, purpose='{} active bond interface'.format(master_name))
-                    logging.info('selected {} active bond interface: {}'.format(master_name, master_active_bond_device))
-                    master_active_bond_enumeration = get_device_enumeration(master_active_bond_device, os='rhcos')
-                    logging.info('{} active bond enumeration: {}'.format(master_name, master_active_bond_enumeration))
-                    master_backup_bond_device = generate_network_devices_menu(map_devices, purpose='{} backup bond interface'.format(master_name))
-                    logging.info('selected {} backup bond interface: {}'.format(master_name, master_backup_bond_device))
-                    master_backup_bond_enumeration = get_device_enumeration(master_backup_bond_device, os='rhcos')
-                    logging.info('{} backup bond enumeration: {}'.format(master_name, master_backup_bond_enumeration))
-                    self.set_network_details(node_type='master_nodes', node_name=master_name, ip=master_os_ip, mac=master_mac,
-                                             bond_name=master_bond_name, primary=master_active_bond_enumeration,
-                                             backup=master_backup_bond_enumeration)
-
-                else:
-                    master_nic_device = generate_network_devices_menu(map_devices, purpose='{} nic port'.format(master_name))
-                    logging.info('selected {} as nic port: {}'.format(master_name, master_nic_device))
-                    master_nic_device_enumeration = get_device_enumeration(master_nic_device, os='rhcos')
-                    logging.info('{} nic device enumeration: {}'.format(master_name, master_nic_device_enumeration))
-                    master_nic_mac = get_mac_address(master_nic_device, base_api_url, user, passwd)
-                    logging.info('{} nic mac address: {}'.format(master_name, master_nic_mac))
-                    node_keys = ['name','ip','mac','interface']
-                    node_values = [master_name,master_os_ip,master_nic_mac,master_nic_device_enumeration]
-                    logging.info('{} node values: {}'.format(master_name, node_values))
-                    node_pairs = dict(zip(node_keys, node_values))
-                    self.inventory_dict['csah']['vars']['master_nodes'].append(node_pairs)
-
-
-            self.clear_screen()
-        self.inventory_dict['csah']['vars']['number_of_masters'] = master_nodes_count
+        self.inventory_dict = get_nodes_info(node_type='master', inventory=self.inventory_dict)
 
     def get_worker_nodes(self):
         """ 
@@ -254,107 +188,7 @@ class InventoryFile:
 
         """
         self.clear_screen()
-        worker_nodes_count = input('enter number of worker nodes\n'
-                                   'default [3]: ')
-        default = 3
-        worker_nodes_count = set_values(worker_nodes_count, default, check='integer')
-        self.inventory_dict['csah']['vars']['worker_nodes'] = []
-        bonding = input('Do you want to perform bonding (y/NO): ')
-        valid_responses = ['y', 'NO']
-
-        while bonding not in valid_responses:
-            logging.info('Invalid option provided. Enter \'y\' or \'NO\'')
-            bonding = input('Do you want to perform bonding (y/NO): ')
-
-        for num in range(worker_nodes_count):
-            worker_values = []
-            worker_devices = None
-            worker_interfaces_enumeration = []
-            worker_mac = ''
-
-            default = 'worker-{}'.format(num)
-            worker_name = input('enter the worker {} node name \n'
-                                'default [{}]: '.format(num, default))
-            worker_name = set_values(worker_name, default)
-
-            supported_os = ['rhel','rhcos']
-            logging.info('supported OS include {}'.format(supported_os)) 
-            worker_os = input('enter the worker OS: ')
-
-            while worker_os not in supported_os:
-                logging.error('Supported OS should be \'rhcos\' or \'rhel\'')
-                worker_os = input('enter the worker OS: ')
-
-            worker_os = set_values(worker_os, default)            
-            worker_os_ip = get_ip(node_name=worker_name, ip_type='os')
-            worker_os_ip = validate_ip(worker_os_ip)
-            worker_idrac_ip = get_ip(node_name=worker_name, ip_type='idrac')
-            response = check_ip_ping(worker_idrac_ip)
-            
-            if response != 0:
-                get_user_response(message='idrac ip {} not pingeable'.format(worker_idrac_ip))
-            else:
-                user, passwd = get_idrac_creds(worker_idrac_ip)
-                base_api_url = 'https://{}/redfish/v1/Systems/System.Embedded.1/EthernetInterfaces'.format(worker_idrac_ip)
-                worker_devices = get_network_devices(user, passwd, base_api_url)
-
-            if worker_devices:
-                map_devices = map_interfaces_network(worker_devices)
-
-            if map_devices:
-                if bonding == 'y':
-                    worker_mac = get_network_device_mac(map_devices, user, passwd, base_api_url)
-                    worker_bond_name = 'bond0'
-                    worker_active_bond_device = generate_network_devices_menu(map_devices, purpose='{} active bond interface'.format(worker_name))
-                    logging.info('selected {} active bond interface: {}'.format(worker_name, worker_active_bond_device))
-                    worker_active_bond_enumeration = get_device_enumeration(worker_active_bond_device, os=worker_os)
-                    logging.info('{} active bond enumeration: {}'.format(worker_name, worker_active_bond_enumeration))
-                    worker_backup_bond_device = generate_network_devices_menu(map_devices, purpose='{} backup bond interface'.format(worker_name))
-                    logging.info('selected {} backup bond interface: {}'.format(worker_name, worker_backup_bond_device))
-                    worker_backup_bond_enumeration = get_device_enumeration(worker_backup_bond_device, os=worker_os)
-                    logging.info('{} backup bond enumeration: {}'.format(worker_name, worker_backup_bond_enumeration))
-                    logging.info('interfaces: {}'.format(worker_devices))
-                    logging.info('map interfaces: {}'.format(map_devices))
-                    
-                    if worker_os == 'rhel':
-                        for worker_device in map_devices:
-                            interface_enumeration = get_device_enumeration(worker_device, os=worker_os)
-                            worker_interfaces_enumeration.append(interface_enumeration)
-                    else:
-                        worker_interfaces_enumeration.append(worker_active_bond_enumeration)
-                
-                    self.set_network_details(node_type='worker_nodes', node_name=worker_name, ip=worker_os_ip, mac=worker_mac,
-                                             bond_name=worker_bond_name, primary=worker_active_bond_enumeration,
-                                             backup=worker_backup_bond_enumeration, interfaces=worker_interfaces_enumeration)
-                
-                else:
-                    worker_nic_device = generate_network_devices_menu(map_devices, purpose='{} nic port'.format(worker_name))
-                    logging.info('selected {} as nic port: {}'.format(worker_name, worker_nic_device))
-                    worker_nic_device_enumeration = get_device_enumeration(worker_nic_device, os=worker_os)
-                    logging.info('{} nic device enumeration: {}'.format(worker_name, worker_nic_device_enumeration))
-                    worker_nic_mac = get_mac_address(worker_nic_device, base_api_url, user, passwd)
-                    logging.info('{} nic mac address: {}'.format(worker_name, worker_nic_mac))
-                    node_keys = ['name','ip','mac','interface','os']
-
-                    if worker_os == 'rhel':
-                        logging.info('entered if condition')
-                        node_keys = ['name','ip','mac','interface','os','interfaces']
-                        for worker_device in map_devices:
-                            interface_enumeration = get_device_enumeration(worker_device, os='rhel')
-                            worker_interfaces_enumeration.append(interface_enumeration)
-                        node_values = [worker_name,worker_os_ip,worker_nic_mac,worker_nic_device_enumeration,worker_os,worker_interfaces_enumeration]
-                    else:
-                        logging.info('entered else condition')
-                        worker_interfaces_enumeration.append(worker_nic_device_enumeration) 
-                        node_values = [worker_name,worker_os_ip,worker_nic_mac,worker_nic_device_enumeration,worker_os]
-                    logging.info('{} node values: {}'.format(worker_name, node_values))
-                    node_pairs = dict(zip(node_keys, node_values))
-                    self.inventory_dict['csah']['vars']['worker_nodes'].append(node_pairs)
-
-
-            self.clear_screen()
-
-        self.inventory_dict['csah']['vars']['number_of_workers'] = worker_nodes_count
+        self.inventory_dict = get_nodes_info(node_type='worker', inventory=self.inventory_dict)
 
     def dhcp_lease_times(self):
         """ 
@@ -364,34 +198,6 @@ class InventoryFile:
         self.clear_screen()
         self.inventory_dict['csah']['vars']['default_lease_time'] = 8000
         self.inventory_dict['csah']['vars']['max_lease_time'] = 72000
-
-    def set_network_details(self, node_type='', node_name='', ip='', mac='', bond_name='', primary='', backup='', interfaces=''):
-        """ 
-        get bond details and user interfaces used for bond
-
-        """
-        devices = []
-        self.clear_screen()
-        node_keys = ['name','ip','mac','bond','primary','backup','options']
-        node_values = []
-        bond_options = 'lacp_rate=1,miimon=100,mode=802.3ad,xmit_hash_policy=layer3+4'
-        bond_interfaces = '{},{}'.format(primary, backup)
-        node_values.append(node_name)
-        node_values.append(ip)
-        node_values.append(mac)
-        node_values.append(bond_name)
-        node_values.append(primary)
-        node_values.append(backup)
-        node_values.append(bond_options)
-
-        if node_type == 'worker_nodes':
-            node_keys = ['name','ip','mac','bond','primary','backup','options', 'interfaces']
-            node_values.append(interfaces)
-            logging.info('adding interfaces in {} node: {}'.format(node_name, interfaces))
-
-        node_pairs = dict(zip(node_keys, node_values))
-        logging.info('node_values {} {} {}'.format(node_type, node_values, node_pairs))
-        self.inventory_dict['csah']['vars'][node_type].append(node_pairs)
 
     def get_dns_details(self):
         """ 
